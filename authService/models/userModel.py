050c95c7e3cb12d773df049fcd56391ef112277c
Camilo Peña <nestor.pena@usantoto.edu.co>
2026-05-14 11:24:44 -0500

    fix(auth): renombrar tabla a auth_users para resolver colisión con users-service en PostgreSQL

--- authService/models/userModel.py
+++ authService/models/userModel.py
@@ -9,7 +9,7 @@ class Role(db.Model):
 
 
 class User(db.Model):
-    __tablename__ = "users"
+    __tablename__ = "auth_users"
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
